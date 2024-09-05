2e6097b39d0c60046d298bff760f33b6f0de7cbf
justmatedev <justmatedev@gmail.com>
2024-09-05 19:26:24 -0300

    up

---
+++ fda/BasicTypeScript/build/aula1.js
@@ -0,0 +1,8 @@
+"use strict";
+const nome = "mateus";
+console.log("bem vindo " + nome);
+function sum(a) {
+    return console.log(a);
+}
+sum(5);
+sum(4);
---
+++ fda/BasicTypeScript/src/aula1.ts
@@ -0,0 +1,11 @@
+const nome = "mateus"
+
+console.log("bem vindo " + nome)
+
+function sum(a: number) {
+  return console.log(a)
+}
+
+// comentario aquiiiiiiii
+sum(5)
+sum(4)
